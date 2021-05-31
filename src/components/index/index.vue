b476229d0da4beadebc2e439704027a4c38a4a09
wk0924 <1366576343@qq.com>
2021-05-31 21:24:11 +0800

    .

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,12 +1,6 @@
 	<template>
   <div class="container">
-    <el-menu
-      class="topNav"
-      mode="horizontal"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-    >
+    <el-menu class="topNav" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
       <el-menu-item index="1">编辑</el-menu-item>
       <el-menu-item index="2">预览</el-menu-item>
       <el-menu-item index="3">清空</el-menu-item>
